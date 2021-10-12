0f8ede96f7a677480d23c02b1c3bdad535697879
akhmad aziz mario <12201084@bsi.ac.id>
2021-10-12 08:14:40 +0700

    update terbaru

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -14,41 +14,73 @@
 
     <div class="row-mt-3">
         <div class="colmd-6">
-            <br><a href="http://localhost/phpmvc/barang/tambah" class="btn btn-primary">Tambah Data Barang</a>
+            <br><a href="http://localhost/phpmvc/barang/tambah" class="btn btn-success">Tambah Data Barang</a>
         </div>
     </div>
+    <br><br>
     <div class="row mt-3">
         <div class="col-md-6">
             <form action="" method="post">
                 <div class="input-group">
                     <input type="text" class="form-control" placeholder="Cari Data Barang..." name="keyword">
-                    <button class="btn btn-primary" type="submit">Cari</button>
+                    <button class="btn btn-info" type="submit">Cari</button>
                 </div>
         </div>
     </div>
-    </form>
+    </form><br>
     <table class="table">
         <thead>
             <tr>
+                <th>
                 <th scope="col">Kode Barang</th>
+                </th>
+                <th>
                 <th scope="col">Nama Barang</th>
+                </th>
+                <th>
+                <th>
                 <th scope="col">Harga</th>
+                </th>
+                </th>
+                <th>
+                <th>
                 <th scope="col">Stok</th>
+                </th>
+                </th>
+                <th>
+                <th>
+                <th scope="col">action</th>
+                </th>
+                </th>
             </tr>
         </thead>
-        <br>
         <tbody>
             <tr>
                 <?php foreach ($barang as $brg) : ?>
+                    <th>
                     <td><?= $brg['id_barang']; ?></td>
+                    </th>
+                    <th>
                     <td><?= $brg['nama_barang']; ?></td>
+                    </th>
+                    <th>
+                    <th>
                     <td><?= $brg['harga']; ?></td>
+                    </th>
+                    </th>
+                    <th>
+                    <th>
                     <td><?= $brg['stok']; ?></td>
-
+                    </th>
+                    </th>
+                    <th>
+                    <th>
                     <td><a href="http://localhost/phpmvc/barang/detail/<?= $brg['id_barang']; ?>" class="btn btn-primary">Detail</a>
                         <a href="http://localhost/phpmvc/barang/ubah/<?= $brg['id_barang']; ?>" class="btn btn-success">Ubah</a>
                         <a href="http://localhost/phpmvc/barang/hapus/<?= $brg['id_barang']; ?>" class="btn btn-danger" onclick="return confirm ('Yakin akan dihapus?');">Hapus</a>
                     </td>
+                    </th>
+                    </th>
             </tr>
         <?php endforeach; ?>
 
