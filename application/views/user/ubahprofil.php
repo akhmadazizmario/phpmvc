380d3b1a343de88fe7a109fa4510a86b9ea5be88
akhmad aziz mario <12201084@bsi.ac.id>
2021-12-04 07:42:48 +0700

    upload terbaru

--- application/views/user/ubahprofil.php
+++ application/views/user/ubahprofil.php
@@ -1,10 +1,11 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
+
     <div class="row">
         <div class="col-lg-9">
             <?= form_open_multipart('user/ubahprofil'); ?>
             <div class="form-group row">
-                <label for="email" class="col-sm-2 col-formlabel">Email</label>
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                 </div>
@@ -13,7 +14,7 @@
                 <label for="nama" class="col-sm-2 col-form-label">Nama Lengkap</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="name" name="name" value="<?= $user['name']; ?>">
-                    <?= form_error('name', '<small class="text-danger pl3">', '</small>'); ?>
+                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                 </div>
             </div>
             <div class="form-group row">
@@ -21,7 +22,7 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail">
+                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-thumbnail" alt="">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
@@ -32,16 +33,20 @@
                     </div>
                 </div>
             </div>
+
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
                     <button type="submit" class="btn btn-primary">Ubah</button>
                     <button class="btn btn-dark" onclick="window.history.go(-1)"> Kembali</button>
                 </div>
             </div>
+
             </form>
         </div>
     </div>
+
 </div>
 <!-- /.container-fluid -->
+
 </div>
 <!-- End of Main Content -->
